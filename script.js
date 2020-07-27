8bbdfca716c8a45f3503237c7398ab7a53b2b255
mozahidul01 <mozo.postbox@gmail.com>
2020-07-28 01:32:56 +0600

    Total Add

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
+// Input Getting Function
+
+function getInputValue(product) {
+    const productInput = document.getElementById(product + '-count');
+    const productCount = parseInt(productInput.value);
+    return productCount;
+}
+
+// Product Increase And Decrease
 
 function handleProductChange(product, isAdd) {
-    const productNumber = document.getElementById(product + '-count');
-    const productCount = parseInt(productNumber.value);
+    const productCount = getInputValue(product);
 
     let productNewCount = productCount;
     if (isAdd == true) {
@@ -10,8 +18,7 @@ function handleProductChange(product, isAdd) {
     if (isAdd == false && productCount > 0) {
         productNewCount = productCount - 1;
     }
-
-    productNumber.value = productNewCount;
+    document.getElementById(product + '-count').value = productNewCount;
 
     let productNewPrice = 0;
     if (product == 'phone') {
@@ -23,5 +30,30 @@ function handleProductChange(product, isAdd) {
 
     const productPrice = document.getElementById(product + '-price');
     productPrice.innerText = productNewPrice;
+    calculateTotal();
 }
 
+// Total Calculation Function
+
+function calculateTotal() {
+    const phoneCount = getInputValue('phone');
+    const caseCount = getInputValue('case');
+
+    const totalPrice = phoneCount * 749 + caseCount * 39;
+    document.getElementById('total-price').innerText = totalPrice;
+
+    const totalTax = (totalPrice * 0.05).toFixed(0);
+    document.getElementById('total-tax').innerText = totalTax;
+
+    const grandPrice = totalTax + totalPrice;
+    console.log(grandPrice);
+    document.getElementById('grand-price').innerText = grandPrice;
+}
+
+// Remove product
+
+const removeBtn = document.getElementById('phone-remove');
+removeBtn.addEventListener("click", function (event) {
+    const removeArea = document.getElementById('phone');
+    removeArea.style.display = "none";
+})
